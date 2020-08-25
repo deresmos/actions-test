f74dda80980f68d177b865c793ae7bffccea4d1c
deresmos <deresmos@gmail.com>
2020-08-25 19:41:43 +0900

    okok

--- installer/pyenv.sh
+++ installer/pyenv.sh
@@ -7,5 +7,7 @@ if type pyenv > /dev/null; then
 fi
 
 curl -sL https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-exec $SHELL
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 git clone https://github.com/massongit/pyenv-pip-update $(pyenv root)/plugins/pyenv-pip-update
