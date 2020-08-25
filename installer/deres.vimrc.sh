514ea7e46b2c48014412de56a5a98425fc0ecf99
deresmos <deresmos@gmail.com>
2020-08-25 20:14:02 +0900

    kok

--- installer/deres.vimrc.sh
+++ installer/deres.vimrc.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
 git clone https://github.com/deresmos/deres.vimrc ~/myself/deres.vimrc
-cd ~/myself/deres.vimrc/setup-scripts
+
+cd ~/myself/deres.vimrc
+make symbolic
+
+cd setup-scripts
 bash inst-cmigemo.sh
 bash inst-pip-package.sh
 bash inst-npm-package.sh
