f5089fef0b145e2cb9f1a6de6d1ca29534892a8b
deresmos <deresmos@gmail.com>
2020-08-25 22:38:48 +0900

    tmp

---
+++ installer/all.sh
@@ -0,0 +1,20 @@
+# curl pyenv
+pyenv install 3.8.5
+pyenv virtualenv 3.8.5 main
+pyenv global main
+
+# for mac
+# brew
+# brew_list
+# brew cask install amethyst
+# brew cask install background-music
+
+# neovim
+
+# nodenv
+nodenv install 14.8.0
+nodenv global 14.8.0
+
+# ricty
+# nerdfonts
+# deres.vimrc
